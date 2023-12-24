9678ee88a80a21554f7f6eefb70ae3370382dc41
DylanAlloy <alloydylan@icloud.com>
2023-12-24 13:49:01 -0500

    📖 fix(deps): PyYAML

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        "yaml"
+        "PyYAML"
     ],
     url = 'https://github.com/Prismadic/hygiene'
 )
